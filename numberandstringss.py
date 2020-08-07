2f8b7668c80b375fb53c28b4a0f47db98a939001
Nathan Ferguson <nathan@home.lab>
2020-08-07 10:16:22 -0700

    Basic tutorial

---
+++ numberandstringss.py
@@ -0,0 +1,36 @@
+myint = 7
+print (myint)
+
+myfloat = 7.0
+print (myfloat)
+myfloat = float(7)
+print (myfloat)
+
+mystring = 'hello'
+print(mystring)
+mystring = "hello"
+print(mystring)
+
+mystring = "Don't worry about apostrophes"
+print(mystring)
+
+one = 1
+two = 2
+three = one + two
+print (three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+a, b = 3, 4
+print (a,b)
+
+# this will not work
+one = 1
+two = 2
+hello = "hello"
+
+print(one + two + hello)
+
